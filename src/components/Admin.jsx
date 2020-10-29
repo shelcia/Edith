7cfbf9b0fe1b81edf2482e9c7fd414915c1ff92a
noob <shelcia1999@gmail.com>
2020-10-29 16:01:17 +0530

    bug fix

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Loading from "./Loading";
 
 const AdminDashboard = () => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const URL = process.env.REACT_APP_HEROKU_LINK;
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`${URL}api/auth/users`)
       .then((response) => {
         console.log(response.data);
+        setIsLoading(false);
         setUsers(response.data);
       })
       .catch((error) => console.log(error));
@@ -32,73 +36,77 @@ const AdminDashboard = () => {
   return (
     <React.Fragment>
       <div className='container my-5 shadow p-4 rounded'>
-        <div className='container' id='accordion'>
-          {users.map((user) => (
-            <div
-              className='card mb-4'
-              key={user._id}
-              style={{ height: "75vh", overflowY: "scroll" }}>
-              <div className='card-header'>
-                <a className='card-link' data-toggle='collapse' href='#name'>
-                  {user.name}
-                </a>
-              </div>
-              <div className='card-body'>
-                <p>Email: {user.email}</p>
-              </div>
-              <div className='card-body mb-4'>
-                <p className='text-primary'>All Submissions</p>
-              </div>
-              {user.submission.map((sub) => (
-                <div className='mb-4' key={sub.timeStamp}>
-                  <table className='table table-hover w-75 mx-auto shadow'>
-                    <tbody>
-                      <tr>
-                        <th>Hint Title: </th>
-                        <td>{sub.hintTitle}</td>
-                      </tr>
-                      <tr>
-                        <th>Submitted Answer: </th>
-                        <td>{sub.answer}</td>
-                      </tr>
-                      <tr>
-                        <th>Result: </th>
-                        <td>{sub.isCorrect ? "Correct" : " Wrong"}</td>
-                      </tr>
-                      <tr>
-                        <th>Time Stamp: </th>
-                        <td>
-                          {sub.timeStamp ? convertDate(sub.timeStamp) : ""}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className='container' id='accordion'>
+            {users.map((user) => (
+              <div
+                className='card mb-4'
+                key={user._id}
+                style={{ height: "75vh", overflowY: "scroll" }}>
+                <div className='card-header'>
+                  <a className='card-link' data-toggle='collapse' href='#name'>
+                    {user.name}
+                  </a>
                 </div>
-              ))}
-              <div className='card-body mb-4'>
-                <p className='text-primary'>All Opened Hints</p>
-              </div>
-              {user.isHintOpen.map((hint) => (
-                <div className='mb-4' key={hint.timeStamp}>
-                  <table className='table table-hover w-75 mx-auto shadow'>
-                    <tbody>
-                      <tr>
-                        <th>Hint Title: </th>
-                        <td>{hint.hintTitle}</td>
-                      </tr>
-                      <tr>
-                        <th>Time Stamp: </th>
-                        <td>
-                          {hint.timeStamp ? convertDate(hint.timeStamp) : " "}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                <div className='card-body'>
+                  <p>Email: {user.email}</p>
                 </div>
-              ))}
-            </div>
-          ))}
-        </div>
+                <div className='card-body mb-4'>
+                  <p className='text-primary'>All Submissions</p>
+                </div>
+                {user.submission.map((sub) => (
+                  <div className='mb-4' key={sub.timeStamp}>
+                    <table className='table table-hover w-75 mx-auto shadow'>
+                      <tbody>
+                        <tr>
+                          <th>Hint Title: </th>
+                          <td>{sub.hintTitle}</td>
+                        </tr>
+                        <tr>
+                          <th>Submitted Answer: </th>
+                          <td>{sub.answer}</td>
+                        </tr>
+                        <tr>
+                          <th>Result: </th>
+                          <td>{sub.isCorrect ? "Correct" : " Wrong"}</td>
+                        </tr>
+                        <tr>
+                          <th>Time Stamp: </th>
+                          <td>
+                            {sub.timeStamp ? convertDate(sub.timeStamp) : ""}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                ))}
+                <div className='card-body mb-4'>
+                  <p className='text-primary'>All Opened Hints</p>
+                </div>
+                {user.isHintOpen.map((hint) => (
+                  <div className='mb-4' key={hint.timeStamp}>
+                    <table className='table table-hover w-75 mx-auto shadow'>
+                      <tbody>
+                        <tr>
+                          <th>Hint Title: </th>
+                          <td>{hint.hintTitle}</td>
+                        </tr>
+                        <tr>
+                          <th>Time Stamp: </th>
+                          <td>
+                            {hint.timeStamp ? convertDate(hint.timeStamp) : " "}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </React.Fragment>
   );
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const Loading = ({ message }) => {
   return (
     <React.Fragment>
-      <div className='spinner-grow text-info'></div>
-      <div className='spinner-grow text-warning'></div>
+      <div className='spinner-grow text-warning mr-3'></div>
       <div className='spinner-grow text-danger'></div>
       <p>{message}</p>
     </React.Fragment>
